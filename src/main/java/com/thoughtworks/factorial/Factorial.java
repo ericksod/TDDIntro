5777d8dcccd9912339af1f231ef73d6c6db3e5aa
Danielle Erickson <e.erickson.d@gmail.com>
2014-09-18 00:34:40 -0400

    made throw illegal arg exception when number is negative

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.factorial;
 
+import java.util.IllegalFormatCodePointException;
+
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException{
+        if(i<0){
+            throw new IllegalArgumentException("input was negative, expected a positive number");
+        }
         if (i==0){
             return 1;
         }else if(i==3){
